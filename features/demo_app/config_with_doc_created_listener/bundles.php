8a837a9de8ab802e4a30936965b13107d57e48c6
Yoanm <yoanm@users.noreply.github.com>
2018-11-01 20:23:53 +0100

    Improve

--- features/demo_app/config_with_doc_created_listener/bundles.php
+++ features/demo_app/config_with_doc_created_listener/bundles.php
@@ -3,5 +3,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Yoanm\SymfonyJsonRpcHttpServer\JsonRpcHttpServerBundle::class => ['all' => true],
     Yoanm\SymfonyJsonRpcHttpServerDoc\JsonRpcHttpServerDocBundle::class => ['all' => true],
-    \Yoanm\SymfonyJsonRpcHttpServerOpenAPIDoc\JsonRpcHttpServerOpenAPIDocBundle::class => ['all' => true],
+    Yoanm\SymfonyJsonRpcHttpServerOpenAPIDoc\JsonRpcHttpServerOpenAPIDocBundle::class => ['all' => true],
 ];
