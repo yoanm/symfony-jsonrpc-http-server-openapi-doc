4310144a14bb0a8e29d5ebc8aa3ea4873f487cb8
Yoanm <yoanm@users.noreply.github.com>
2020-01-12 14:29:15 +0100

    Fix

--- tests/Common/DependencyInjection/AbstractTestClass.php
+++ tests/Common/DependencyInjection/AbstractTestClass.php
@@ -19,7 +19,7 @@ abstract class AbstractTestClass extends AbstractExtensionTestCase
     /**
      * {@inheritdoc}
      */
-    protected function getContainerExtensions()
+    protected function getContainerExtensions(): array
     {
         return [
             new JsonRpcHttpServerDocExtension(),
